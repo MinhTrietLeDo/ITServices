26086933e41519ba0e3245c83c8b79b23d740a3f
YamadaAkai <trietldm1705@gmail.com>
2023-08-26 11:00:47 +0700

    rẻ nhánh để test các kiểu idk

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
 export const API_URL = 'http://172.16.18.45/apirest.php'
-export const App_Token = 'fZcc1o1hBSveh8cYIIzxmTocr5BbOAHn1ubXcoq8'
+export const App_Token = 'fZcc1o1hBSveh8cYIIzxmTocr5BbOAHn1ubXcoq8' 
