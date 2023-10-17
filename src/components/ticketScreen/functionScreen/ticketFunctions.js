19d43db3bd1e10708f41acc1774c6834aeb36f46
YamadaAkai <trietldm1705@gmail.com>
2023-10-17 16:05:11 +0700

    deo biet nua

--- src/components/ticketScreen/functionScreen/ticketFunctions.js
+++ src/components/ticketScreen/functionScreen/ticketFunctions.js
@@ -1,8 +1,19 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 
 export const SelectUser = async () => {
-    
+    const dispatch = useDispatch()
+    useEffect(() => {
+
+    }, [])
+
+    const getTechnician = async () => {
+        let objHeader = {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            'App-Token': App_Token,
+        };
+    }
 }
 
 export const EditTicket = async () => {
