d8eb0750025cf1cf7e7897b52675d881ee727c7b
YamadaAkai <trietldm1705@gmail.com>
2024-01-17 14:39:34 +0700

    reup app token

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
 export const API_URL = 'http://172.16.18.45/apirest.php'
-export const App_Token = 'dxaf1Ffm0Yyps78I3TR1kCyFUm4MvGISgO5TsHqS' 
+export const App_Token = 'dFbvWFfPUvy2yH9JytXxgrs7wWlj2UFK3iN6M1LE' 
