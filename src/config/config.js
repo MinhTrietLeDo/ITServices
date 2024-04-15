ca9d12c0459275ceb5ba29e66237e9d3bd165a09
YamadaAkai <trietldm1705@gmail.com>
2024-04-15 07:54:21 +0700

    change ip, app_token, google services.json

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://172.16.18.45/apirest.php'
-export const App_Token = 'dFbvWFfPUvy2yH9JytXxgrs7wWlj2UFK3iN6M1LE' 
+export const API_URL = 'http://172.16.18.182/apirest.php/'
+export const App_Token = 'xsARKOMPGQNGyUAB5gsBA44tde2CvCzyIWlhD7iP' 
