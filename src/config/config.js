630857906aa65c4051c43d81d2562e2945fc424a
YamadaAkai <trietldm1705@gmail.com>
2024-04-25 08:32:51 +0700

    slight update

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
 export const API_URL = 'http://172.16.18.182/apirest.php/'
-export const App_Token = 'xsARKOMPGQNGyUAB5gsBA44tde2CvCzyIWlhD7iP' 
+export const App_Token = 'rdh0dpMo767n485nFWYBNcZrnHzyUhIwArdBIf0t' 
